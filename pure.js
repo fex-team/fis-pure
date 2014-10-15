2e8eb43df79483d06aa4a8add74e994f4e822c45
蹬 <qdsang@gmail.com>
2014-10-15 22:51:04 +0800

    Update pure.js

--- pure.js
+++ pure.js
@@ -62,7 +62,8 @@ fis.config.merge({
                 //前端模板
                 reg : '**.tmpl',
                 //当做类js文件处理，可以识别__inline, __uri等资源定位标识
-                isJsLike : true,
+                isJsonLike : true,
+                isHtmlLike : true,
                 //只是内嵌，不用发布
                 release : false
             },
