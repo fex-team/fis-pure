775adfd059375c07db50de7823e5b984250df3b5
hefangshi <hefangshi@gmail.com>
2015-03-25 14:33:03 +0800

    fix roadmap.path

--- pure.js
+++ pure.js
@@ -63,7 +63,7 @@ fis.config.merge({
             //只是内嵌，不用发布
             release: false
         }, {
-            reg: /.*\.(html|jsp|tpl|vm|htm|asp|aspx|php)/,
+            reg: /.*\.(html|jsp|tpl|vm|htm|asp|aspx|php)$/,
             useCache: false,
             release: '$&'
         }, {
